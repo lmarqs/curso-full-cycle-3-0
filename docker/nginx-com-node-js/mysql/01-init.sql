ab9f539f8b981819097bb9a096228480ae030c17
lmarqs <marques.analista@gmail.com>
2022-09-09 17:49:30 -0300

    run db and app using docker compose

---
+++ docker/nginx-com-node-js/mysql/01-init.sql
@@ -0,0 +1,19 @@
+USE app;
+
+CREATE TABLE names (
+    id INTEGER AUTO_INCREMENT,
+    name TEXT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO names (name) VALUES 
+('David de Souza'),
+('Ashley Ferraz'),
+('Camilo Torres'),
+('Isadora Vila'),
+('Pâmela Prado'),
+('Adriana Gil'),
+('Ian Valentin'),
+('Mia Marinho'),
+('Allison Torres'),
+('Alma Valência');
