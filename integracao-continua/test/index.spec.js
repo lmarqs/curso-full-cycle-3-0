811d10b2cdfe5e84cd862fff754bbc9a8ed6c71f
lmarqs <marques.analista@gmail.com>
2022-09-10 17:22:49 -0300

    feat: add multiply feat

--- integracao-continua/test/index.spec.js
+++ integracao-continua/test/index.spec.js
@@ -1,5 +1,9 @@
-const { sum } = require("../lib/index");
+const { sum, multiply } = require("../lib/index");
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('multiply 1 * 2 to equal 2', () => {
+    expect(multiply(1, 2)).toBe(2);
 });
